b050e1b04faa9c8b0c33a0ae9099326a9a150b26
Yogesh Bhambhu <yogeshbhambhu81@gmail.com>
2026-03-18 10:19:35 +0530

    fix file don't show error

--- frontend/src/pages/professor.jsx
+++ frontend/src/pages/professor.jsx
@@ -11,7 +11,7 @@ const getInitialAuth = () => {
   }
 };
 
-const API_BASE_URL = "http://localhost:5000/api/professor";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function ProfessorDashboard() {
   const [auth, setAuth] = useState(getInitialAuth());
